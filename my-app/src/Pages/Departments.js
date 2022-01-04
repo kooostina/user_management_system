5957091c569cc18a0f18d9e02fa04ac351b83a5f
Olha Kostina <o.kostina777@gmail.com>
2022-01-04 15:59:08 +0200

    started creating protected route

--- my-app/src/Pages/Departments.js
+++ my-app/src/Pages/Departments.js
@@ -1,15 +1,67 @@
 import React, { Component } from "react";
+// import { Link } from "react-router-dom";
 
 class Departments extends Component {
-  render() {
-    return (
-      <div>
-        <div>
-          <h2>Departments</h2>
-        </div>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+    };
   }
+
+  componentDidMount() {
+    fetch("localhost:3000/api/v1/departments")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map((item) => (
+            <li key={item.id}>
+              {item.name} {item.description}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+  // clickHandler() {
+  //   console.log("Clicked!");
+  //   <Link href="./employees"></Link>;
+  // }
+
+  // render() {
+  //   return (
+  //     <div>
+  //       <div>
+  //         <button onClick={this.clickHandler}>See all employees</button>
+  //       </div>
+  //     </div>
+  //   );
+  // }
 }
 
 export default Departments;
---
+++ my-app/src/Pages/ProtectedRoute.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Route, Redirect } from 'react-router-dom';
+
+class ProtectedRoute extends Component {
+  render() {
+    return (
+      <Route/>
+    );
+  }
+}
+
+export default ProtectedRoute;
+
